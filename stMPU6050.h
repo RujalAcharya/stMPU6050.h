0d47fb7378ff0146857c54765f61ac90923633d0
Rujal Acharya <076bei029.rujal@pcampus.edu.np>
2022-06-17 00:26:03 +0545

    Add basic .ioc project

---
+++ stMPU6050.h
@@ -0,0 +1,14 @@
+/*
+    stMPU6050.h
+    A scuffed header file library to use MPU6050 with STM32
+*/
+
+#ifndef __STMPU6050_H
+#define __STMPU6050_H
+
+#include "i2c.h"
+
+// Include this line based on the STM32 Microcontroller you are using
+#include "stm32f4xx.h"  
+
+#endif
